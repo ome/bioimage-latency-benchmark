c91e95ddbed99046019b6dbcc84c50ad7d50163c
jmoore <josh@glencoesoftware.com>
2021-01-09 18:14:41 +0100

    Add HDF5 example

--- t.py
+++ t.py
@@ -1,6 +1,8 @@
 import time
 
 import boto3
+import fsspec
+import h5py
 import pytest
 import requests
 import s3fs
@@ -18,20 +20,32 @@ b3 = boto3.client(
 )
 
 
-def local(filename):
+def local(filename, loader=None):
     start = time.time()
     with open(f"data/{filename}", "rb") as o:
-        o.read()
+        if loader:
+            loader(o)
+        else:
+            o.read()
     stop = time.time()
     return stop - start
 
 
-def http(filename):
-    elapsed = requests.get(f"http://localhost:8000/{filename}").elapsed
-    return elapsed.microseconds / 1000000
+def http(filename, loader=None):
+    url = f"http://localhost:8000/{filename}"
+    if loader:
+        with fsspec.open(url) as f:
+            loader(f)
+    else:
+        elapsed = requests.get(f"http://localhost:8000/{filename}").elapsed
+        return elapsed.microseconds / 1000000
 
 
-def boto3(filename):
+def boto3(filename, loader=None):
+
+    if loader is not None:
+        pytest.skip()
+
     start = time.time()
     rsp = b3.get_object(Bucket="data", Key=filename)
     rsp["Body"].read()
@@ -39,10 +53,13 @@ def boto3(filename):
     return stop - start
 
 
-def s3fs(filename):
+def s3fs(filename, loader=None):
     start = time.time()
     with fs.open(f"data/{filename}") as f:
-        f.read()
+        if loader:
+            loader(f)
+        else:
+            f.read()
     stop = time.time()
     return stop - start
 
@@ -59,4 +76,9 @@ def test_zarr_chunk(benchmark, method):
 
 @pytest.mark.parametrize("method", (local, http, boto3, s3fs))
 def test_hdf5_chunk(benchmark, method):
-    benchmark(method, "a.ome.zarr/0/0.0.0.0.0")
+    def loader(opened_file):
+        with h5py.File(opened_file) as f:
+            data = f["DataSet"]["ResolutionLevel 0"]["TimePoint 0"]["Channel 0"]["Data"]
+            len(data[0:16, 0:256, 0:256])  # FIXME - not first chunk
+
+    benchmark(method, "retina_large.ims", loader)
