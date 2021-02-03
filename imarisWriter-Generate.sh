ef00cb65d8f60fcc03b5c363eced9262d67c2746
jmoore <josh@glencoesoftware.com>
2021-02-03 06:56:33 +0000

    Restrict to 2 use-cases (bad alloc)

--- imarisWriter-Generate.sh
+++ imarisWriter-Generate.sh
@@ -11,22 +11,25 @@ echo **********************************
 
 cd ImarisWriterTest/application
 mkdir ${OUTPUT}
-for (( x=1024; x<=16384; x=x*16))
-do
-  for (( z=1; z<=1024; z=z*16))
-  do
-    for (( t=1; t<=1024; t=t*16))
-    do
-      for (( c=1; c<=3; c++))
-      do
-        for (( xc=32; xc<=1024; xc=xc*8))
-        do
-          for (( zc=1; zc<=64 && zc <= z; zc=zc*8))
-          do
-            ./ImarisWriterTestRelease -sizex $x -sizey $x -sizez $z -sizet $t -sizec $c -chunkx $xc -chunky $xc -chunkz $zc -type 16bit -threads 8 -outputpath ${OUTPUT}  IMS_XY-$x-Z-$z-T-$t-C-$c-XYC-$xc-ZC-$zc.ims
-          done
-        done
-      done
-    done
-  done
-done
+
+convert(){
+    ./ImarisWriterTestRelease -sizex $x -sizey $x -sizez $z -sizet $t -sizec $c -chunkx $xc -chunky $xc -chunkz $zc -type 16bit -threads 8 -outputpath ${OUTPUT}  IMS_XY-$x-Z-$z-T-$t-C-$c-XYC-$xc-ZC-$zc.ims
+}
+
+# https://github.com/ome/ngff-latency-benchmark/issues/5
+x=131072
+z=131072
+c=32
+t=1
+xc=1024
+zc=1024
+convert
+
+# https://github.com/ome/ngff-latency-benchmark/issues/6
+x=512
+z=512
+c=2
+t=512
+xc=64
+zc=64
+convert
