a52acb1c70f3f2bf866bd583e824e0aaae262666
jmoore <josh@glencoesoftware.com>
2021-02-10 10:23:21 +0000

    Setup for large 2D trial

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,117 +1,80 @@
 import time
 
-import boto3
 import fsspec
 import h5py
 import pytest
 import requests
 import s3fs
 import tifffile
-from botocore import UNSIGNED
-from botocore.client import Config
+import zarr
 from os import environ
 
 DIR = environ.get("DIR", "data")
 BASE = environ.get("BASE", "retina_large")
 HOST = environ.get("HOST", "localhost")
 
-fs = s3fs.S3FileSystem(
-    anon=True, client_kwargs={"endpoint_url": f"http://{HOST}:9000"}
-)
 
-b3 = boto3.client(
-    "s3",
-    endpoint_url=f"http://{HOST}:9000",
-    config=Config(signature_version=UNSIGNED),
-)
+def local(filename: str) -> str:
+    return f"{DIR}/{filename}", fsspec.filesystem('file')
 
 
-def local(filename, loader=None):
-    start = time.time()
-    with open(f"{DIR}/{filename}", "rb") as o:
-        if loader:
-            loader(o)
-        else:
-            o.read()
-    stop = time.time()
-    return stop - start
+def http(filename: str) -> str:
+    return f"http://{HOST}:8000/{filename}", fsspec.filesystem('http')
 
 
-def http(filename, loader=None):
-    url = f"http://{HOST}:8000/{filename}"
-    if loader:
-        with fsspec.open(url) as f:
-            loader(f)
-    else:
-        elapsed = requests.get(url).elapsed
-        return elapsed.microseconds / 1000000
+def s3(filename: str) -> str:
+    return f"s3://data/{filename}", s3fs.S3FileSystem(
+        anon=True, client_kwargs={"endpoint_url": f"http://{HOST}:9000"})
 
 
-def boto3(filename, loader=None):
-
-    if loader is not None:
-        pytest.skip()
-
-    start = time.time()
-    rsp = b3.get_object(Bucket="data", Key=filename)
-    rsp["Body"].read()
-    stop = time.time()
-    return stop - start
-
-
-def s3fs(filename, loader=None):
-    start = time.time()
-    with fs.open(f"data/{filename}") as f:
-        if loader:
-            loader(f)
-        else:
-            f.read()
-    stop = time.time()
-    return stop - start
-
-
-@pytest.mark.parametrize("method", (local, http, boto3, s3fs))
+@pytest.mark.parametrize("method", (local, http, s3))
 def test_1_byte_overhead(benchmark, method):
-    benchmark(method, "1-byte")
+
+    def loader(filename: str, fs):
+        with fs.open(filename) as f:
+            f.read()
+        
+    benchmark(loader, *method("1-byte"))
 
 
-@pytest.mark.parametrize("method", (local, http, boto3, s3fs))
+@pytest.mark.parametrize("method", (local, http, s3))
 def test_zarr_chunk(benchmark, method):
-    benchmark(method, f"{BASE}.ome.zarr/0/0/0/0/0/0")
+
+    def loader(filename: str, fs):
+        store = s3fs.S3Map(root=filename, s3=fs, check=False)
+        group = zarr.group(store=store)
+        data = group["0"]
+        chunks = data.chunks
+        len(data[0:chunks[0], 0:chunks[1], 0:chunks[2], 0:chunks[3], 0:chunks[4]])
+
+    benchmark(loader, *method(f"{BASE}.ome.zarr"))
 
 
-@pytest.mark.parametrize("method", (local, http, boto3, s3fs))
+@pytest.mark.parametrize("method", (local, http, s3))
 def test_tiff_tile(benchmark, method):
-    def loader(opened_file):
-        with tifffile.TiffFile(opened_file) as tif:
-            fh = tif.filehandle
-            for page in tif.pages:
-                fh.seek(page.dataoffsets[0])
-                fh.read(page.databytecounts[0])
-                return
 
-    benchmark(method, f"{BASE}.ome.tiff", loader)
+    def loader(filename: str, fs):
+
+        with fs.open(filename) as f:
+            with tifffile.TiffFile(f) as tif:
+                fh = tif.filehandle
+                for page in tif.pages:
+                    fh.seek(page.dataoffsets[0])
+                    fh.read(page.databytecounts[0])
+                    return
+
+    benchmark(loader, *method(f"{BASE}.ome.tiff"))
 
 
-@pytest.mark.parametrize("method", (local, http, boto3, s3fs))
+@pytest.mark.parametrize("method", (local, http, s3))
 def test_hdf5_chunk(benchmark, method):
-    def loader(opened_file):
-        with h5py.File(opened_file) as f:
-            data = f["DataSet"]["ResolutionLevel 0"]["TimePoint 0"]["Channel 0"]["Data"]
-            chunks = data.chunks
-            len(data[0:chunks[0]-1, 0:chunks[1]-1, 0:chunks[2]-1])
 
-    benchmark(method, f"{BASE}.ims", loader)
+    def loader(filename: str, fs):
 
+        with fs.open(filename) as f:
+            with h5py.File(f) as ims:
+                data = ims["DataSet"]["ResolutionLevel 0"]["TimePoint 0"]["Channel 0"]["Data"]
+                chunks = data.chunks
+                len(data[0:chunks[0], 0:chunks[1], 0:chunks[2]])
 
-@pytest.mark.parametrize("method", (local, http, boto3, s3fs))
-def test_download_1(benchmark, method):
-    def loader(opened_file):
-        opened_file.read()
-
-    benchmark(method, f"{BASE}.ims", loader)
-
-
-@pytest.mark.parametrize("method", (local, http, boto3, s3fs))
-def test_download_2(benchmark, method):
-    benchmark(method, f"{BASE}.ims")
+    benchmark(loader, *method(f"{BASE}.ims"))
--- upload.sh
+++ upload.sh
@@ -9,7 +9,8 @@ set -e
 set -u
 set -x
 
-mc config host add benchmark http://$HOST:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
+# Avoiding nginx for upload
+mc config host add benchmark http://minio1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 mc mb -p benchmark/data
 mc policy set public benchmark/data
 
