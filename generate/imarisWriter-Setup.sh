5e12e8aee5474c9e903e2005eee9496ef59c03c1
jmoore <josh@glencoesoftware.com>
2021-03-03 09:27:17 +0100

    generation updates on testing gradient

--- generate/imarisWriter-Setup.sh
+++ generate/imarisWriter-Setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #hdf5
 
+set -e
+set -u
+set -x
+
 wget -N -O CMake-hdf5-1.12.0.tar.gz "https://www.hdfgroup.org/package/cmake-hdf5-1-12-0-tar-gz/?wpdmdl=14580&refresh=600867a2422561611163554"
 tar -xzf CMake-hdf5-1.12.0.tar.gz
 cd CMake-hdf5-1.12.0
@@ -42,9 +46,10 @@ make install
 cd ../..
 
 #imarisWriterTest
-wget -N -O ImarisWriterTest.zip https://github.com/dgault/ImarisWriterTest/archive/ngff-benchmark-gen.zip
+BRANCH=ngff-benchmark-gen
+wget -N -O ImarisWriterTest.zip https://github.com/dgault/ImarisWriterTest/archive/${BRANCH}.zip
 unzip ImarisWriterTest.zip
-mv ImarisWriterTest-ngff-benchmark-gen ImarisWriterTest
+mv ImarisWriterTest-${BRANCH} ImarisWriterTest
 cd ImarisWriterTest/application
 clang++ -std=c++11  -I../.. -L../../ImarisWriter/release/lib -o ImarisWriterTestRelease ImarisWriterTest.cxx -lbpImarisWriter96 -lpthread -rpath ../../CMake-hdf5-1.12.0/HDF_Group/HDF5/1.12.0/lib
 cd ../..
