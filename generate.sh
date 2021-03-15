de99aa2d7262d54d4d2733f2f8f28425526b589e
jmoore <josh@glencoesoftware.com>
2021-03-15 16:16:43 +0000

    WIP: failing

--- generate.sh
+++ generate.sh
@@ -15,19 +15,23 @@ TIFF=IMS_XY-${XY}-Z-${Z}-T-${T}-C-${C}-XYC-${XC}-ZC-${ZC}.ome.tiff
 BF2RAW=/tmp/bioformats2raw/build/install/bioformats2raw/bin/bioformats2raw
 RAW2OMETIFF=/tmp/raw2ometiff/build/install/raw2ometiff/bin/raw2ometiff
 
-docker-compose run ${CLEAN} -v $DIR:$DIR \
+echo IMS
+time docker-compose run ${CLEAN} -v $DIR:$DIR \
 	generate $DIR \
 	$XY $Z $C $T $XC $ZC
 
-docker-compose run ${CLEAN} -v $DIR:$DIR \
+echo ZARR
+time docker-compose run ${CLEAN} -v $DIR:$DIR \
 	convert $BF2RAW \
 		--nested \
+		--chunk_depth $ZC \
 		--compression=raw \
 		$DIR/$IMS \
 		$DIR/out \
 	-w $XC -h $XC
 
-docker-compose run ${CLEAN} -v $DIR:$DIR \
+echo TIFF
+time docker-compose run ${CLEAN} -v $DIR:$DIR \
 	convert $RAW2OMETIFF \
 		--compression=raw \
 		$DIR/out \
