327226971bd2cf087b4e3cc65a9cb9beb1c45ced
jmoore <josh@glencoesoftware.com>
2021-04-07 14:06:59 +0100

    Exclude extraneous files on upload

--- upload.sh
+++ upload.sh
@@ -16,4 +16,8 @@ mc config host add benchmark http://minio1:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECR
 mc mb -p benchmark/${BUCKET}
 mc policy set public benchmark/${BUCKET}
 
-time mc mirror --overwrite ${DIR} benchmark/${BUCKET}/${NAME}/
+time mc mirror --overwrite  \
+	--exclude "out/*" \
+	--exclude "*.csv" \
+	--exclude ".*bfmemo"  \
+	${DIR} benchmark/${BUCKET}/${NAME}/
