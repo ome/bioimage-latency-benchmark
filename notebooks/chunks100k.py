275f15bb32aac6b2926ac942a13f7fe72b539a70
jmoore <josh@glencoesoftware.com>
2021-04-09 10:24:04 +0200

    Cleanup new figure scripts

--- notebooks/chunks100k.py
+++ notebooks/chunks100k.py
@@ -1,10 +1,9 @@
-from ipywidgets import *
-import numpy as np
-import matplotlib.pyplot as plt
 import math
 
-x = 2**15
-y = 2**15
+import matplotlib.pyplot as plt
+
+x = 2 ** 15
+y = 2 ** 15
 z = 1
 t = 1
 c = 25
@@ -14,12 +13,19 @@ chunkZ = 1
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-plt.xlabel('Chunk size (X and Y)')
-plt.ylabel('Number of chunks')
+plt.xlabel("Chunk size (X and Y)")
+plt.ylabel("Number of chunks")
 plt.title("XYZCT: 32k x 32k x 1 x 25 x 1")
 
+
 def file_count(i, chunkC=1):
-    return math.ceil(x/i) * math.ceil(y/i) * math.ceil(z/1) * math.ceil(t/1) * math.ceil(c/chunkC)
+    return (
+        math.ceil(x / i)
+        * math.ceil(y / i)
+        * math.ceil(z / 1)
+        * math.ceil(t / 1)
+        * math.ceil(c / chunkC)
+    )
 
 
 for chunkC in (1, 5, 25):
@@ -32,14 +38,21 @@ for chunkC in (1, 5, 25):
 
 plt.legend(loc="lower left", title="Chunk size (C)", frameon=False)
 
+
 def scale(scale):
     plt.yscale(scale)
     plt.xscale(scale)
 
-scale('log')
-ax.annotate('Chosen chunk size\nC: 1, X: 256, Y: 256',
-        xy=((256), file_count(256)), xycoords='data',
-        xytext=(0, 40), textcoords='offset points',
-        arrowprops=dict(facecolor='black', shrink=0.05),
-        horizontalalignment='left', verticalalignment='bottom')
+
+scale("log")
+ax.annotate(
+    "Chosen chunk size\nC: 1, X: 256, Y: 256",
+    xy=((256), file_count(256)),
+    xycoords="data",
+    xytext=(0, 40),
+    textcoords="offset points",
+    arrowprops=dict(facecolor="black", shrink=0.05),
+    horizontalalignment="left",
+    verticalalignment="bottom",
+)
 fig.savefig("2d-chunks.png", dpi=800)
--- notebooks/chunks3d.py
+++ notebooks/chunks3d.py
@@ -1,7 +1,6 @@
-from ipywidgets import *
-import numpy as np
-import matplotlib.pyplot as plt
 import math
+
+import matplotlib.pyplot as plt
 
 x = 1024
 y = 1024
@@ -12,12 +11,20 @@ chunkSizes = (16, 32, 64, 128)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-plt.xlabel('Chunk size (XYZ)')
-plt.ylabel('Number of chunks')
-plt.title("XYZCT: 1k x 1k x 1k x 1 x 100");
+plt.xlabel("Chunk size (XYZ)")
+plt.ylabel("Number of chunks")
+plt.title("XYZCT: 1k x 1k x 1k x 1 x 100")
+
 
 def file_count(i, chunkT=1):
-    return math.ceil(x/i) * math.ceil(y/i) * math.ceil(z/i) * math.ceil(t/chunkT) * math.ceil(c/1)
+    return (
+        math.ceil(x / i)
+        * math.ceil(y / i)
+        * math.ceil(z / i)
+        * math.ceil(t / chunkT)
+        * math.ceil(c / 1)
+    )
+
 
 for chunkT in (1, 10, 100):
     numFiles = []
@@ -25,18 +32,25 @@ for chunkT in (1, 10, 100):
     for i in chunkSizes:
         numFiles.__iadd__([file_count(i, chunkT)])
         fileSize.__iadd__([i])
-    line, = ax.plot(fileSize, numFiles, label=f"{chunkT}")
+    (line,) = ax.plot(fileSize, numFiles, label=f"{chunkT}")
 
 plt.legend(loc="lower left", title="Chunk sizes (T)", frameon=False)
+
 
 def scale(scale):
     plt.yscale(scale)
     plt.xscale(scale)
 
-scale('log')
-ax.annotate('Chosen chunk size\nT: 1, X: 64, Y: 64, Z: 64',
-        xy=((64), file_count(64, 1)), xycoords='data',
-        xytext=(0, 40), textcoords='offset points',
-        arrowprops=dict(facecolor='black', shrink=0.05),
-        horizontalalignment='left', verticalalignment='bottom')
-fig.savefig('3d-chunks.png', dpi=800)
+
+scale("log")
+ax.annotate(
+    "Chosen chunk size\nT: 1, X: 64, Y: 64, Z: 64",
+    xy=((64), file_count(64, 1)),
+    xycoords="data",
+    xytext=(0, 40),
+    textcoords="offset points",
+    arrowprops=dict(facecolor="black", shrink=0.05),
+    horizontalalignment="left",
+    verticalalignment="bottom",
+)
+fig.savefig("3d-chunks.png", dpi=800)
